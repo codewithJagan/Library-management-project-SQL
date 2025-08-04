edcb5fcc0ed31972f0d087d7de745de1dc0e62aa
Hara Prasad Behera <123396656+codewithJagan@users.noreply.github.com>
2025-08-04 22:57:20 +0530

    Add files via upload

---
+++ Question with answer SQL.sql
@@ -0,0 +1,289 @@
+-- Library System Management SQL Project
+
+-- CREATE DATABASE library;
+
+-- Create table "Branch"
+DROP TABLE IF EXISTS branch;
+CREATE TABLE branch
+(
+            branch_id VARCHAR(10) PRIMARY KEY,
+            manager_id VARCHAR(10),
+            branch_address VARCHAR(30),
+            contact_no VARCHAR(15)
+);
+
+
+-- Create table "Employee"
+DROP TABLE IF EXISTS employees;
+CREATE TABLE employees
+(
+            emp_id VARCHAR(10) PRIMARY KEY,
+            emp_name VARCHAR(30),
+            position VARCHAR(30),
+            salary DECIMAL(10,2),
+            branch_id VARCHAR(10),
+            FOREIGN KEY (branch_id) REFERENCES  branch(branch_id)
+);
+
+
+-- Create table "Members"
+DROP TABLE IF EXISTS members;
+CREATE TABLE members
+(
+            member_id VARCHAR(10) PRIMARY KEY,
+            member_name VARCHAR(30),
+            member_address VARCHAR(30),
+            reg_date DATE
+);
+
+
+
+-- Create table "Books"
+DROP TABLE IF EXISTS books;
+CREATE TABLE books
+(
+            isbn VARCHAR(50) PRIMARY KEY,
+            book_title VARCHAR(80),
+            category VARCHAR(30),
+            rental_price DECIMAL(10,2),
+            status VARCHAR(10),
+            author VARCHAR(30),
+            publisher VARCHAR(30)
+);
+
+
+
+-- Create table "IssueStatus"
+DROP TABLE IF EXISTS issued_status;
+CREATE TABLE issued_status
+(
+            issued_id VARCHAR(10) PRIMARY KEY,
+            issued_member_id VARCHAR(30),
+            issued_book_name VARCHAR(80),
+            issued_date DATE,
+            issued_book_isbn VARCHAR(50),
+            issued_emp_id VARCHAR(10),
+            FOREIGN KEY (issued_member_id) REFERENCES members(member_id),
+            FOREIGN KEY (issued_emp_id) REFERENCES employees(emp_id),
+            FOREIGN KEY (issued_book_isbn) REFERENCES books(isbn) 
+);
+
+
+
+-- Create table "ReturnStatus"
+DROP TABLE IF EXISTS return_status;
+CREATE TABLE return_status
+(
+            return_id VARCHAR(10) PRIMARY KEY,
+            issued_id VARCHAR(30),
+            return_book_name VARCHAR(80),
+            return_date DATE,
+            return_book_isbn VARCHAR(50),
+            FOREIGN KEY (return_book_isbn) REFERENCES books(isbn)
+);
+
+SELECT * FROM books ;
+
+--Task 1. Create a New Book Record -- "978-1-60129-456-2', 'To Kill a Mockingbird', 'Classic', 6.00, 'yes', 'Harper Lee', 'J.B. Lippincott & Co.')"
+
+INSERT INTO books(isbn,book_title,category,rental_price,status,author,publisher)
+VALUES ('978-1-60129-456-2', 'To Kill a Mockingbird', 'Classic', 6.00, 'yes', 'Harper Lee', 'J.B. Lippincott & Co.');
+
+--Task 2: Update an Existing Member's Address
+UPDATE members
+SET member_address = '125 Oak St'
+WHERE member_id = 'C103';
+
+--Task 3: Delete a Record from the Issued Status Table -- Objective: Delete the record with issued_id = 'IS121' from the issued_status table.
+DELETE FROM issued_status
+WHERE issued_id='IS121';
+
+
+
+--Task 4: Retrieve All Books Issued by a Specific Employee -- Objective: Select all books issued by the employee with emp_id = 'E101'.
+SELECT * FROM issued_status
+WHERE issued_emp_id = 'E101'
+
+
+--Task 5: List Members Who Have Issued More Than One Book -- Objective: Use GROUP BY to find members who have issued more than one book.
+SELECT
+    issued_emp_id,
+    COUNT(*)
+FROM issued_status
+GROUP BY 1
+HAVING COUNT(*)> 4
+
+--TASK-6
+
+SELECT 
+  b.isbn,
+  b.book_title,
+  
+  COUNT (ist.issued_id) as no_issued
+
+FROM books as b
+JOIN
+issued_status as ist
+ON ist.issued_book_isbn= b.isbn
+
+GROUP BY 1
+
+
+-- SELECT b.isbn, b.book_title, COUNT(ist.issued_id) AS issue_count
+-- FROM issued_status as ist
+-- JOIN books as b
+-- ON ist.issued_book_isbn = b.isbn
+-- GROUP BY b.isbn, b.book_title;
+
+
+--TASK 8 Find Total Rental Income by Category:
+SELECT * FROM books
+SELECT * FROM issued_status
+SELECT 
+    b.category,
+    SUM(b.rental_price),
+    COUNT(*)
+FROM 
+books as b
+GROUP BY b.category;
+
+--TASK 9 List Members Who Registered in the Last 456 Days:
+
+SELECT * FROM members
+WHERE reg_date >= CURRENT_DATE - INTERVAL '456 days';
+
+-- TASK 10 List Employees with Their Branch Manager's Name and their branch details:
+
+SELECT * FROM branch
+SELECT 
+     e1.*,b.manager_id,e2.emp_name as manager 
+FROM employees as e1
+JOIN branch as b
+ON b.branch_id = e1.branch_id
+JOIN employees as e2
+ON b.manager_id = e2.emp_id
+
+
+--TASK 11 Retrive  Books with Rental Price Above a Certain Threshold: USD 7
+
+SELECT * FROM books
+WHERE rental_price > 7.00;
+
+
+--Task 12: Retrieve the List of Books Not Yet Returned
+
+SELECT * FROM return_status
+
+SELECT DISTINCT ist.issued_book_name
+FROM issued_status as ist
+LEFT JOIN
+return_status as rs
+ON rs.issued_id = ist.issued_id
+WHERE rs.return_id IS NULL;
+
+---Task 13: Identify Members with Overdue Books
+--Write a query to identify members who have overdue books (assume a 30-day return period). Display the member's_id, member's name, book title, issue date, and days overdue.---
+
+SELECT 
+    ist.issued_member_id,
+    m.member_name,
+    bk.book_title,
+    ist.issued_date,
+    -- rs.return_date,
+    CURRENT_DATE - ist.issued_date as over_dues_days
+FROM issued_status as ist
+JOIN 
+members as m
+    ON m.member_id = ist.issued_member_id
+JOIN 
+books as bk
+ON bk.isbn = ist.issued_book_isbn
+LEFT JOIN 
+return_status as rs
+ON rs.issued_id = ist.issued_id
+WHERE 
+    rs.return_date IS NULL
+    AND
+    (CURRENT_DATE - ist.issued_date) > 30
+ORDER BY 1
+
+
+--TASK 14 Write a query to update the status of books in the books table to "Yes" when they are returned (based on entries in the return_status table).
+
+
+CREATE OR REPLACE PROCEDURE add_return_records(p_return_id VARCHAR(10), p_issued_id VARCHAR(10), p_book_quality VARCHAR(10))
+LANGUAGE plpgsql
+AS $$
+
+DECLARE
+    v_isbn VARCHAR(50);
+    v_book_name VARCHAR(80);
+    
+BEGIN
+    -- all your logic and code
+    -- inserting into returns based on users input
+    INSERT INTO return_status(return_id, issued_id, return_date, book_quality)
+    VALUES
+    (p_return_id, p_issued_id, CURRENT_DATE, p_book_quality);
+
+    SELECT 
+        issued_book_isbn,
+        issued_book_name
+        INTO
+        v_isbn,
+        v_book_name
+    FROM issued_status
+    WHERE issued_id = p_issued_id;
+
+    UPDATE books
+    SET status = 'yes'
+    WHERE isbn = v_isbn;
+
+    RAISE NOTICE 'Thank you for returning the book: %', v_book_name;
+    
+END;
+$$
+
+
+-- Testing FUNCTION add_return_records
+
+issued_id = IS135
+ISBN = WHERE isbn = '978-0-307-58837-1'
+
+SELECT * FROM books
+WHERE isbn = '978-0-307-58837-1';
+
+SELECT * FROM issued_status
+WHERE issued_book_isbn = '978-0-307-58837-1';
+
+SELECT * FROM return_status
+WHERE issued_id = 'IS135';
+
+-- calling function 
+CALL add_return_records('RS138', 'IS135', 'Good');
+
+-- calling function 
+CALL add_return_records('RS148', 'IS140', 'Good');
+
+
+
+--TASK 15 Find Employees with the Most Book Issues Processed
+
+SELECT 
+    e.emp_name,
+    b.*,
+    COUNT(ist.issued_id) as no_book_issued
+FROM issued_status as ist
+JOIN
+employees as e
+ON e.emp_id = ist.issued_emp_id
+JOIN
+branch as b
+ON e.branch_id = b.branch_id
+GROUP BY 1, 2
+
+
+
+
+
+
